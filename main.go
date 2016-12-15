b203934eb5a5c3b3a75cf539db3b8178f89005e7
d.sevillano <diego.sevillano@sainsburys.co.uk>
2016-12-15 15:06:36 +0000

    Solution lesson 3

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/chazsmi/mapsexcerise/people"
+)
+
 func main() {
+	channel := make(chan []string)
+	go people.CharliesTeam(channel)
+	go people.PeopleOnFloorFive(channel)
+	go people.PeopleWorkingInProduct(channel)
+
+	for i := 0; i < 3; i++ {
+		fmt.Printf("Result %s\n", <-channel)
+	}
 }
